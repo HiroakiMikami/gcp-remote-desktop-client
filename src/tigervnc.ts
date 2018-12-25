bac49bddfd90251d04ca521bac0ace78f879afb6
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-12-25 00:57:47 +0000

    Add VncViewerBuilder class

--- src/tigervnc.ts
+++ src/tigervnc.ts
@@ -1,5 +1,8 @@
+import { Command } from "commander"
+import * as os from "os"
+import * as path from "path"
 import { doNothing, toFunction } from "./utils"
-import { IVncViewer } from "./vnc_viewer"
+import { IVncViewer, IVncViewerBuilder } from "./vnc_viewer"
 
 export interface IOptions {
     passwordFile?: string
@@ -34,5 +37,27 @@ export class VncViewer implements IVncViewer<IOptions> {
         }
         args.push(`::${port}`)
         return this.vncViewerCommand(args)
+    }
+}
+
+export class VncViewerBuilder implements IVncViewerBuilder {
+    public commandLineArguments(command: Command): Command {
+        return command
+            .option("--vncviewer-path <command>", "The path of `vncviewer` command", "vncviewer")
+            .option("--password-file <password-file>", "The path of vnc password file",
+                path.join(os.homedir(), ".vnc", "passwd"))
+            .option("--quality-level <q>", "The JPEG quality level, 0 = Low, 9 = High", undefined)
+            .option("--compress-level <c>", "The compression level, 0 = Low, 9 = High", undefined)
+        }
+    public create(command: Command): IVncViewer<void> {
+        const viewer = new VncViewer(command.vncviewer_path)
+        return {
+            connect(port: number, _: void): Promise<Error> {
+                return viewer.connect(port, {
+                    compressLevel: command.compressLevel,
+                    passwordFile: command.passwordFile,
+                    qualityLevel: command.qualityLevel })
+            },
+        }
     }
 }
