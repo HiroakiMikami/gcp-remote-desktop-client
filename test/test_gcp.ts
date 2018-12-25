13f5e6da2697aab76b21af93b44febf7d53e942c
Hiroaki Mikami <hiroaki8270@gmail.com>
2018-12-25 08:18:34 +0000

    Modify the test case names

--- test/test_gcp.ts
+++ test/test_gcp.ts
@@ -3,149 +3,151 @@ const should = chai.should()
 
 import { Cloud } from "../src/gcp"
 
-describe("Cloud", () => {
-    describe("#createInstance", () => {
-        it("create a VM and start it", async () => {
-            const history: Array<{}> = []
-            const gcp = new Cloud((args) => {
-                history.push(args)
-                return Promise.resolve("result")
+describe("GCP", () => {
+    describe("Cloud", () => {
+        describe("#createInstance", () => {
+            it("create a VM and start it", async () => {
+                const history: Array<{}> = []
+                const gcp = new Cloud((args) => {
+                    history.push(args)
+                    return Promise.resolve("result")
+                })
+                await gcp.createMachine("test", { machineType: "n1-highmem-4" })
+                history.length.should.equal(2)
+                history[0].should.deep.equal([
+                    "beta", "compute", "instances", "create",
+                    "test", "--machine-type=n1-highmem-4",
+                    "--disk=name=test,device-name=test,mode=rw,boot=yes"])
+                history[1].should.deep.equal(["compute", "instances", "start", "test"])
             })
-            await gcp.createMachine("test", { machineType: "n1-highmem-4" })
-            history.length.should.equal(2)
-            history[0].should.deep.equal([
-                "beta", "compute", "instances", "create",
-                "test", "--machine-type=n1-highmem-4",
-                "--disk=name=test,device-name=test,mode=rw,boot=yes"])
-            history[1].should.deep.equal(["compute", "instances", "start", "test"])
-        })
-        it("specify a custum machine-type", async () => {
-            const history: Array<{}> = []
-            const gcp = new Cloud((args) => {
-                history.push(args)
-                return Promise.resolve("result")
+            it("specify a custum machine-type", async () => {
+                const history: Array<{}> = []
+                const gcp = new Cloud((args) => {
+                    history.push(args)
+                    return Promise.resolve("result")
+                })
+                await gcp.createMachine("test", { machineType: { vCPU: 24, memory: 100 } })
+                history[0].should.deep.equal([
+                    "beta", "compute", "instances", "create",
+                    "test", "--machine-type=custum-24-102400",
+                    "--disk=name=test,device-name=test,mode=rw,boot=yes"])
             })
-            await gcp.createMachine("test", { machineType: { vCPU: 24, memory: 100 } })
-            history[0].should.deep.equal([
-                "beta", "compute", "instances", "create",
-                "test", "--machine-type=custum-24-102400",
-                "--disk=name=test,device-name=test,mode=rw,boot=yes"])
-        })
-        it("add accelerator", async () => {
-            const history: Array<{}> = []
-            const gcp = new Cloud((args) => {
-                history.push(args)
-                return Promise.resolve("result")
+            it("add accelerator", async () => {
+                const history: Array<{}> = []
+                const gcp = new Cloud((args) => {
+                    history.push(args)
+                    return Promise.resolve("result")
+                })
+                await gcp.createMachine("test",
+                                        {
+                                            accelerators: [{ deviceType: "nvidia-tesla-k80", count: 1}],
+                                            machineType: "n1-highmem-4",
+                                        })
+                history[0].should.deep.equal([
+                    "beta", "compute", "instances", "create",
+                    "test",
+                    "--accelerator", "type=nvidia-tesla-k80,count=1",
+                    "--machine-type=n1-highmem-4",
+                    "--disk=name=test,device-name=test,mode=rw,boot=yes"])
             })
-            await gcp.createMachine("test",
-                                    {
-                                        accelerators: [{ deviceType: "nvidia-tesla-k80", count: 1}],
-                                        machineType: "n1-highmem-4",
-                                    })
-            history[0].should.deep.equal([
-                "beta", "compute", "instances", "create",
-                "test",
-                "--accelerator", "type=nvidia-tesla-k80,count=1",
-                "--machine-type=n1-highmem-4",
-                "--disk=name=test,device-name=test,mode=rw,boot=yes"])
-        })
-        it("specify the tags", async () => {
-            const history: Array<{}> = []
-            const gcp = new Cloud((args) => {
-                history.push(args)
-                return Promise.resolve("result")
+            it("specify the tags", async () => {
+                const history: Array<{}> = []
+                const gcp = new Cloud((args) => {
+                    history.push(args)
+                    return Promise.resolve("result")
+                })
+                await gcp.createMachine("test", { machineType: "n1-highmem-4", tags: ["foo", "bar"] })
+                history[0].should.deep.equal([
+                    "beta", "compute", "instances", "create",
+                    "test", "--tags=foo,bar", "--machine-type=n1-highmem-4",
+                    "--disk=name=test,device-name=test,mode=rw,boot=yes"])
             })
-            await gcp.createMachine("test", { machineType: "n1-highmem-4", tags: ["foo", "bar"] })
-            history[0].should.deep.equal([
-                "beta", "compute", "instances", "create",
-                "test", "--tags=foo,bar", "--machine-type=n1-highmem-4",
-                "--disk=name=test,device-name=test,mode=rw,boot=yes"])
-        })
-        it("specify the zone", async () => {
-            const history: Array<{}> = []
-            const gcp = new Cloud((args) => {
-                history.push(args)
-                return Promise.resolve("result")
+            it("specify the zone", async () => {
+                const history: Array<{}> = []
+                const gcp = new Cloud((args) => {
+                    history.push(args)
+                    return Promise.resolve("result")
+                })
+                await gcp.createMachine("test", { machineType: "n1-highmem-4", zone: "zone" })
+                history[0].should.deep.equal([
+                    "beta", "compute", "instances", "create",
+                    "test", "--zone=zone", "--machine-type=n1-highmem-4",
+                    "--disk=name=test,device-name=test,mode=rw,boot=yes"])
+                history[1].should.deep.equal([
+                    "compute", "instances", "start",
+                    "test", "--zone=zone"])
             })
-            await gcp.createMachine("test", { machineType: "n1-highmem-4", zone: "zone" })
-            history[0].should.deep.equal([
-                "beta", "compute", "instances", "create",
-                "test", "--zone=zone", "--machine-type=n1-highmem-4",
-                "--disk=name=test,device-name=test,mode=rw,boot=yes"])
-            history[1].should.deep.equal([
-                "compute", "instances", "start",
-                "test", "--zone=zone"])
         })
-    })
 
-    describe("#getPublicIpAddress", () => {
-        it("query the public IP address of the VM", async () => {
-            const gcp = new Cloud((args) => {
-                args.should.deep.equal(["compute", "instances", "list",
-                    "--filter=\"name=test\"",
-                    "--format='value(networkInterfaces[0].accessConfigs[0].natIP)'"])
-                return Promise.resolve("result")
+        describe("#getPublicIpAddress", () => {
+            it("query the public IP address of the VM", async () => {
+                const gcp = new Cloud((args) => {
+                    args.should.deep.equal(["compute", "instances", "list",
+                        "--filter=\"name=test\"",
+                        "--format='value(networkInterfaces[0].accessConfigs[0].natIP)'"])
+                    return Promise.resolve("result")
+                })
+                const result = await gcp.getPublicIpAddress("test", {})
+                result.should.equal("result")
             })
-            const result = await gcp.getPublicIpAddress("test", {})
-            result.should.equal("result")
-        })
-        it("specify the zone", async () => {
-            const gcp = new Cloud((args) => {
-                args.should.deep.equal(["compute", "instances", "list",
-                    "--filter=\"name=test\"",
-                    "--format='value(networkInterfaces[0].accessConfigs[0].natIP)'",
-                    "--zones=zone"])
-                return Promise.resolve("result")
+            it("specify the zone", async () => {
+                const gcp = new Cloud((args) => {
+                    args.should.deep.equal(["compute", "instances", "list",
+                        "--filter=\"name=test\"",
+                        "--format='value(networkInterfaces[0].accessConfigs[0].natIP)'",
+                        "--zones=zone"])
+                    return Promise.resolve("result")
+                })
+                await gcp.getPublicIpAddress("test", { zone: "zone" })
             })
-            await gcp.getPublicIpAddress("test", { zone: "zone" })
         })
-    })
 
-    describe("#terminateInstance", () => {
-        it("stop a VM and delete it", async () => {
-            const history: Array<{}> = []
-            const gcp = new Cloud((args) => {
-                history.push(args)
-                return Promise.resolve("result")
+        describe("#terminateInstance", () => {
+            it("stop a VM and delete it", async () => {
+                const history: Array<{}> = []
+                const gcp = new Cloud((args) => {
+                    history.push(args)
+                    return Promise.resolve("result")
+                })
+                await gcp.terminateMachine("test", {})
+                history.length.should.equal(2)
+                history[0].should.deep.equal([
+                    "compute", "instances", "stop",
+                    "test"])
+                history[1].should.deep.equal(["--quiet", "compute", "instances", "delete",
+                    "--keep-disks", "all", "test"])
             })
-            await gcp.terminateMachine("test", {})
-            history.length.should.equal(2)
-            history[0].should.deep.equal([
-                "compute", "instances", "stop",
-                "test"])
-            history[1].should.deep.equal(["--quiet", "compute", "instances", "delete",
-                "--keep-disks", "all", "test"])
-        })
-        it("specify the zone", async () => {
-            const history: Array<{}> = []
-            const gcp = new Cloud((args) => {
-                history.push(args)
-                return Promise.resolve("result")
+            it("specify the zone", async () => {
+                const history: Array<{}> = []
+                const gcp = new Cloud((args) => {
+                    history.push(args)
+                    return Promise.resolve("result")
+                })
+                await gcp.terminateMachine("test", { zone: "zone" })
+                history.length.should.equal(2)
+                history[0].should.deep.equal([
+                    "compute", "instances", "stop",
+                    "--zone=zone",
+                    "test"])
+                history[1].should.deep.equal(["--quiet", "compute", "instances", "delete",
+                    "--keep-disks", "all", "--zone=zone", "test"])
             })
-            await gcp.terminateMachine("test", { zone: "zone" })
-            history.length.should.equal(2)
-            history[0].should.deep.equal([
-                "compute", "instances", "stop",
-                "--zone=zone",
-                "test"])
-            history[1].should.deep.equal(["--quiet", "compute", "instances", "delete",
-                "--keep-disks", "all", "--zone=zone", "test"])
         })
-    })
 
-    it("return null if the command exists", async () => {
-        const gcp = new Cloud(":")
-        const error = await gcp.terminateMachine("name", {})
-        should.not.exist(error)
-    })
-    it("reject with an error if the command is not found", async () => {
-        const gcp = new Cloud("./not-found")
+        it("return null if the command exists", async () => {
+            const gcp = new Cloud(":")
+            const error = await gcp.terminateMachine("name", {})
+            should.not.exist(error)
+        })
+        it("reject with an error if the command is not found", async () => {
+            const gcp = new Cloud("./not-found")
 
-        try {
-            await gcp.terminateMachine("name", {})
-        } catch (err) {
-            return null
-        }
-        should.exist(null) // Failure  let isCaught = false
+            try {
+                await gcp.terminateMachine("name", {})
+            } catch (err) {
+                return null
+            }
+            should.exist(null) // Failure
+        })
     })
 })
--- test/test_openssh.ts
+++ test/test_openssh.ts
@@ -4,56 +4,57 @@ import * as fs from "fs"
 import * as tmp from "tmp"
 
 import { SshClient } from "../src/openssh"
-
-describe("SshClient", () => {
-    describe("#portForward", () => {
-        it("creates command line arguments for SshClient", () => {
-            const command = new SshClient((args) => {
-                args.should.deep.equal([
-                    "-o", "StrictHostKeyChecking=no", "-fNT", "-p", "22", "-L", "8022:localhost:1022",
-                    "-l", "user", "localhost",
-                ])
-                return Promise.resolve(null)
+describe("OpenSSH", () => {
+    describe("SshClient", () => {
+        describe("#portForward", () => {
+            it("creates command line arguments for SshClient", () => {
+                const command = new SshClient((args) => {
+                    args.should.deep.equal([
+                        "-o", "StrictHostKeyChecking=no", "-fNT", "-p", "22", "-L", "8022:localhost:1022",
+                        "-l", "user", "localhost",
+                    ])
+                    return Promise.resolve(null)
+                })
+                return command.portForward(22, "user", "localhost", 1022, 8022, {})
             })
-            return command.portForward(22, "user", "localhost", 1022, 8022, {})
-        })
-        it("add identity file to command line arguments if identityFile is not null", () => {
-            const command = new SshClient((args) => {
-                args.should.deep.equal([
-                    "-o", "StrictHostKeyChecking=no", "-fNT", "-p", "22", "-L", "8022:localhost:1022",
-                    "-l", "user", "-i", "~/.ssh/id_rsa", "localhost",
-                ])
-                return Promise.resolve(null)
+            it("add identity file to command line arguments if identityFile is not null", () => {
+                const command = new SshClient((args) => {
+                    args.should.deep.equal([
+                        "-o", "StrictHostKeyChecking=no", "-fNT", "-p", "22", "-L", "8022:localhost:1022",
+                        "-l", "user", "-i", "~/.ssh/id_rsa", "localhost",
+                    ])
+                    return Promise.resolve(null)
+                })
+                return command.portForward(22, "user", "localhost", 1022, 8022,
+                                        {identityFile: "~/.ssh/id_rsa" })
             })
-            return command.portForward(22, "user", "localhost", 1022, 8022,
-                                       {identityFile: "~/.ssh/id_rsa" })
-        })
-        it("return onexit function if the command exists", async () => {
-            const command = new SshClient(":")
-            const onexit = await command.portForward(22, "user", "localhost", 22, 8022, {});
-            `${typeof(onexit)}`.toString().should.equal("function")
-        })
-        it("reject with an error if the command is not found", async () => {
-            const command = new SshClient("./not-found")
+            it("return onexit function if the command exists", async () => {
+                const command = new SshClient(":")
+                const onexit = await command.portForward(22, "user", "localhost", 22, 8022, {});
+                `${typeof(onexit)}`.toString().should.equal("function")
+            })
+            it("reject with an error if the command is not found", async () => {
+                const command = new SshClient("./not-found")
 
-            try {
-                await command.portForward(22, "user", "localhost", 22, 8022, {})
-            } catch (err) {
-                return null
-            }
-            should.exist(null) // Failure
-        })
-        it("backup known_hosts file", async () => {
-            const tmpFile = tmp.fileSync()
-            fs.writeFileSync(tmpFile.name, "original")
-            const command = new SshClient((_) => {
-                fs.writeFileSync(tmpFile.name, "foobar")
+                try {
+                    await command.portForward(22, "user", "localhost", 22, 8022, {})
+                } catch (err) {
+                    return null
+                }
+                should.exist(null) // Failure
+            })
+            it("backup known_hosts file", async () => {
+                const tmpFile = tmp.fileSync()
+                fs.writeFileSync(tmpFile.name, "original")
+                const command = new SshClient((_) => {
+                    fs.writeFileSync(tmpFile.name, "foobar")
 
-                return Promise.resolve(null)
-            }, 0, tmpFile.name)
-            const onexit = await command.portForward(22, "user", "localhost", 1022, 8022, {})
-            await onexit()
-            fs.readFileSync(tmpFile.name).toString().should.equal("original")
+                    return Promise.resolve(null)
+                }, 0, tmpFile.name)
+                const onexit = await command.portForward(22, "user", "localhost", 1022, 8022, {})
+                await onexit()
+                fs.readFileSync(tmpFile.name).toString().should.equal("original")
+            })
         })
     })
 })
